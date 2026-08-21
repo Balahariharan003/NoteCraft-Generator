eb68c9e6478c8ec550f71f2b025351769baf965c
Balahariharan003 <balahariharan003@gmail.com>
2026-08-21 19:21:02 +0530

    Revert "Metrix added"
    This reverts commit 46bcadac059431e46981fbb6a93d88e36c67451c.

--- extension-react/src/main.jsx
+++ extension-react/src/main.jsx
@@ -5,6 +5,6 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App mode="popup"/>
+    <App mode="popup" />
   </React.StrictMode>,
 )
